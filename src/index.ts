50bc42549a230e515558462fe18edf0becfc34b2
Rufus Post <rufuspost@gmail.com>
2020-04-08 14:10:15 +1000

    dry up index code

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,23 @@
 import { Connection } from "jsforce";
 
 import { connect, Credentials } from "./connection";
-import { subscribe, Subscription, Message } from "./streaming";
-import { retrieve } from "./retrieve";
-import { list } from "./list";
+import { retrieve as processSync } from "./retrieve";
+import { list as queueupSync } from "./list";
+import {
+  subscribe as queueupSFEventLogs,
+  Subscription,
+  Message
+} from "./streaming";
 
-export { connect, Credentials, Subscription, Message };
-
-export async function processSync<O, M>(
-  conn: Connection,
-  sobjectName: string,
-  sobjectId: string,
-  mapper: (o: O) => M,
-  onReceive: (model: M) => Promise<void>,
-  fields?: string[]
-): Promise<void> {
-  await retrieve<O, M>(conn, sobjectName, sobjectId, mapper, onReceive, fields);
-}
-
-export async function queueupSync(
-  conn: Connection,
-  sobjectName: string,
-  timeout: number,
-  onReceive: (sfid: string) => Promise<void>
-): Promise<void> {
-  await list(conn, sobjectName, timeout, onReceive);
-}
-
-export async function queueupSFEventLogs(
-  conn: Connection,
-  subscription: Subscription,
-  timeout: number,
-  onReceive: (message: Message) => Promise<void>
-): Promise<void> {
-  await subscribe(conn, subscription, timeout, onReceive);
-}
+export {
+  connect,
+  Credentials,
+  Subscription,
+  Message,
+  queueupSync,
+  processSync,
+  queueupSFEventLogs
+};
 
 export async function processSFEventLogs<O, M>(
   conn: Connection,
