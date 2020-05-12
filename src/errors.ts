62f3759d856a468882867630685b646cd223d275
Rufus Post <rufuspost@gmail.com>
2020-05-12 14:25:23 +1200

    drop bulk api for auto fetch (#3)
    Bulk API never seems to fetch large sets and seems more popular as a
    tool for loading data.
    
    This PR changes to use the standard query function using the auto fetch
    parameter.

--- src/errors.ts
+++ src/errors.ts
@@ -1,9 +1,3 @@
-export class FindAllTimeOutError extends Error {
-  constructor(timeout: number) {
-    super(`Bulk query timeout after ${timeout}ms`);
-  }
-}
-
 export class TopicCompileError extends Error {
   constructor(message: string) {
     super(`Topic compile failure: ${message}`);
--- src/findAll.ts
+++ src/findAll.ts
@@ -1,42 +1,30 @@
 import { Connection } from "jsforce";
-import csv from "csv-parser";
 
 import { createSOQL, parseQuery, Query } from "./soql";
-import { FindAllTimeOutError } from "./errors";
 
 export async function find<O, M>(
   conn: Connection,
   sobjectName: string,
-  timeout: number,
+  maxFetch: number,
   mapper: (sobject: O) => M,
   query?: Query
 ): Promise<M[]> {
-  conn.bulk.pollTimeout = timeout;
-
-  const onTimeout = (): void => {
-    throw new FindAllTimeOutError(timeout);
-  };
-
-  const to = setTimeout(onTimeout, timeout);
-
   const { fields, where, limit } = parseQuery(query);
 
   const soql = createSOQL(sobjectName, fields, where, limit);
 
-  const sobjects: O[] = [];
+  const models: M[] = [];
 
   return new Promise((resolve, reject) => {
-    conn.bulk
+    conn
       .query(soql)
-      .stream()
-      .pipe(csv())
-      .on("data", (sobject: O) => {
-        sobjects.push(sobject);
+      .on("record", (sobject: O) => {
+        models.push(mapper(sobject));
       })
       .on("end", async () => {
-        clearTimeout(to);
-        resolve(sobjects.map(mapper));
+        resolve(models);
       })
-      .on("error", reject);
+      .on("error", reject)
+      .run({ autoFetch: true, maxFetch });
   });
 }
